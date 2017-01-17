0d576046ab221655171a2519f3dca8232f995548
benvand <benjaminfvandersteen@gmail.com>
2017-01-17 15:01:02 +0000

    bar

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""
+test a
+"""
+import pip.download
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+
+requirements = list(parse_requirements('requirements.txt',
+                                       session=pip.download.PipSession()))
+
+install_requires = [str(r.req) for r in requirements]
+
+setup(
+    name='test_a',
+    version='1',
+    url='https://github.com/benvand/test_a',
+    license='MIT',
+    author='benvand',
+    description='A',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires
+)
