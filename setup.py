34fc03d3ddba6365dea13f22425e0ea99a32275f
benvand <benjaminfvandersteen@gmail.com>
2017-01-17 16:37:56 +0000

    f

--- setup.py
+++ setup.py
@@ -9,9 +9,7 @@ version='1'
 
 requirements = list(parse_requirements('requirements.txt',
                                        session=pip.download.PipSession()))
-all_dependencies = [str(r.req) for r in requirements]
-install_requires = ['b_test']
-dependency_links = ['git+https://github.com/benvand/b_test.git']
+
 setup(
     name='test_a',
     version='1',
@@ -22,6 +20,6 @@ setup(
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
-    install_requires=install_requires,
-    dependency_links=dependency_links,
+    install_requires=['b_test'],
+    dependency_links=['git+https://github.com/benvand/b_test.git'],
 )
