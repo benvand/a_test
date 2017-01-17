7bc0a73d852a38b99758189f84c9794f0cadb295
benvand <benjaminfvandersteen@gmail.com>
2017-01-17 15:33:51 +0000

    v

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ import pip.download
 from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
+version='1'
 
 requirements = list(parse_requirements('requirements.txt',
                                        session=pip.download.PipSession()))
-
 install_requires = [str(r.req) for r in requirements]
 
 setup(
@@ -21,5 +21,5 @@ setup(
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
-    install_requires=install_requires
+    install_requires=install_requires,
 )
